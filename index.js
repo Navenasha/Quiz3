2452fcf3d227b9709cc71f6a8cb4cdf40057d7d1
Navenasha <navenasha05@gmail.com>
2023-04-11 14:57:50 +0800

    quiz3_wk4

--- index.js
+++ index.js
@@ -1,14 +1,76 @@
+let dbUsers = [
+  {
+      username: "nave",
+      password: "123456",
+      name: "Nave",
+      email: "nave@student.utem.edu.my"
+  },
+
+  {
+      username: "john",
+      password: "password",
+      name:  "John",
+      email: "john@student.utem.edu.my"
+  },
+  
+  {
+      username: "ali",
+      password: "654321",
+      name: "Ali",
+      email: "ali@student.utem.edu.my"
+  },
+]
+
+function login(reqUsername, reqPassword){
+  let matchUser = dbUsers.find (x => x.username == reqUsername)
+
+if(!matchUser) return "User not found!"
+if (matchUser.password == reqPassword){
+  return matchUser
+} else {
+  return "Invalid password"
+}}
+
+
+function register (reqUsername, reqPassword, reqName, reqEmail) {
+dbUsers.push ({
+    username: reqUsername,
+    password: reqPassword,
+    name: reqName,
+    email:reqEmail
+}
+)}
+
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
-app.post('/login', (req, res) => {
+app.post('/login', (req, res) =>{
     console.log(req.body)
-  
-    res.send('Login')
+
+    let result = login(
+      req.body.username,
+      req.body.password
+    )
+
+    res.send('login')
+    })
+
+app.post('/register', (req,res)=>{
+  console.log(req.body)
+    let login = register(
+      req.body.username,
+      req.body.password,
+      req.body.name,
+      req.body.email
+    )
+
+    res.send(login)
   })
+
+
 
 app.get('/', (req, res) => {
   res.send('HAPPY DAY!')
@@ -22,3 +84,5 @@ app.get('/bye', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
